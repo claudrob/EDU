3c2b5ac683267f5208075300285e4196465abfa2
claudrob <crobinson@tec.mx>
2021-09-26 23:52:59 -0500

    Code to teach java

---
+++ src/edu/corejava/ExpressionExample.java
@@ -0,0 +1,20 @@
+package edu.corejava;
+
+public class ExpressionExample {
+
+    // expression = operands & operands
+    // expression = values, variables, numbers, quantity.
+    public static void main(String[] args) {
+
+        int apples = 10;
+        apples = 10 + 5;
+
+        double dbl = ((double)apples) % 3;
+
+        System.out.println(apples);
+        System.out.println(dbl );
+
+
+    }
+
+}
---
+++ src/edu/corejava/GUIExample.java
@@ -0,0 +1,22 @@
+package edu.corejava;
+
+import javax.swing.JOptionPane;
+
+
+public class GUIExample {
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("Enter your name:");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("What is your age"));
+        JOptionPane.showMessageDialog(null, "You are " + age + " years old");
+
+        double height = Double.parseDouble(
+                JOptionPane.showInputDialog("What is your height?")
+        );
+        JOptionPane.showMessageDialog(null, "Your are " + height + " cms tall.");
+
+    }
+
+}
