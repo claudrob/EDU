83d6852b2da6691c6d16a33970702e30120aa942
claudrob <crobinson@tec.mx>
2022-02-04 18:24:56 -0600

    Progress Bar Demo

---
+++ src/my/edu/swing/ProgressBarDemoGUI.java
@@ -0,0 +1,11 @@
+package my.edu.swing;
+
+public class ProgressBarDemoGUI {
+
+    public static void main(String[] args) {
+        ProgresBarDemo demo = new ProgresBarDemo();
+    }
+
+
+
+}
--- src/my/edu/swing/SliderDemoFrame.java
+++ src/my/edu/swing/SliderDemoFrame.java
@@ -15,6 +15,7 @@ public class SliderDemoFrame implements ChangeListener
     public SliderDemoFrame(){
 
         frame = new JFrame("Slider Demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
         panel = new JPanel();
@@ -30,6 +31,15 @@ public class SliderDemoFrame implements ChangeListener
         slider.setMajorTickSpacing(25);
 
         slider.setPaintLabels(true);
+        slider.setFont(new Font("MV Boli", Font.PLAIN, 15));
+        label.setFont(new Font("MV Boli", Font.PLAIN, 15));
+        slider.setOrientation(SwingConstants.VERTICAL);
+
+        label.setText("C = " + slider.getValue());
+
+        slider.addChangeListener(this);
+
+        slider.setPaintLabels(true);
 
         panel.add(slider);
         panel.add(label);
@@ -41,6 +51,8 @@ public class SliderDemoFrame implements ChangeListener
     }
 
     public void stateChanged(ChangeEvent event){
+
+        label.setText("C = " + slider.getValue());
 
     }
 
