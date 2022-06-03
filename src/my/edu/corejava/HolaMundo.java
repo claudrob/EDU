88eaa94498d16f4ddd6524c65d262b001ff0cae9
claudrob <crobinson@tec.mx>
2022-06-03 12:52:10 -0500

    Hola Mundo

---
+++ src/my/edu/corejava/HolaMundo.java
@@ -0,0 +1,19 @@
+package my.edu.corejava;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        int myVariableInteger = 10;
+        System.out.println(myVariableInteger);
+
+        String myVariableString = "Hello";
+        System.out.println(myVariableString);
+
+        myVariableString = "Good bye!";
+
+
+        System.out.println(myVariableString);
+
+
+    }
+}
