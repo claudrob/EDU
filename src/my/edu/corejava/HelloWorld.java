d02147f0a7584b7b2197266f3517c8ac729d286d
claudrob <crobinson@tec.mx>
2022-06-02 19:30:23 -0500

    Hello World 2

--- src/my/edu/corejava/HelloWorld.java
+++ src/my/edu/corejava/HelloWorld.java
@@ -4,11 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-
-        System.out.println("Testing");
-
-        System.out.println("Hello again.");
+        System.out.println("Hola Mundo desde Java!");
 
 
     }
