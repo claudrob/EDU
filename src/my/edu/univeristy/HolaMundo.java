7654afa86e3a2693d597b7db1580fbf460376f11
claudrob <crobinson@tec.mx>
2022-06-04 11:53:08 -0500

    Basic testing

--- src/my/edu/univeristy/HolaMundo.java
+++ src/my/edu/univeristy/HolaMundo.java
@@ -23,6 +23,24 @@ public class HolaMundo {
         var _miVar = 10;
         System.out.print("miVarString: " + miVarString);
 
+        var user = "Claud";
+        var title = "Engineer";
+
+        var union = title + " " + user;
+        System.out.println("Union: " + union);
+
+        var i = 3;
+        var j = 4;
+        System.out.println(i + j);
+        System.out.println(i + j + user);
+        System.out.println(user + (i + j ));
+
+        var name = "Karla";
+        System.out.println("New line: \n" + name);
+        System.out.println("Tab:\t" + name);
+        System.out.println("Retroceso: \b\b\r" + name);
+        System.out.println("Retroceso: \'" + name + "\'");
+        System.out.println("Retroceso: \"" + name + "\"");
 
     }
 }
