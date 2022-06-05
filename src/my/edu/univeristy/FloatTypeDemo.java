f32b07f676955f9f063b61cfd54c6b7098e3a6ea
claudrob <crobinson@tec.mx>
2022-06-05 12:49:28 -0500

    Basic Floats

---
+++ src/my/edu/univeristy/FloatTypeDemo.java
@@ -0,0 +1,26 @@
+package my.edu.univeristy;
+
+public class FloatTypeDemo {
+    public static void main(String[] args) {
+
+        /*
+        Float types
+        - Fl[oat
+        - Double
+         */
+
+        float numberFloat = (float)3.4028235E38D;
+        System.out.println("Number Float: " + numberFloat);
+        System.out.println("Max Float: " + Float.MAX_VALUE);
+        System.out.println("Min Float: " + Float.MIN_VALUE);
+
+        double numberDouble = 1.7976931348623157E308;
+        System.out.println("Number Double: " + numberDouble);
+        System.out.println("Max Float: " + Double.MAX_VALUE);
+        System.out.println("Min Float: " + Double.MIN_VALUE);
+
+
+
+    }
+
+}
