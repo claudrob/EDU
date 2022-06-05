dc9e01c5b88e6ffdfe9442232a11a8399fccee84
claudrob <crobinson@tec.mx>
2022-06-04 19:16:41 -0500

    Basic Ints

--- src/my/edu/univeristy/PrimitivesUNI.java
+++ src/my/edu/univeristy/PrimitivesUNI.java
@@ -14,10 +14,21 @@ public class PrimitivesUNI {
         System.out.println("minimum byte value: " + Byte.MIN_VALUE) ;
         System.out.println("maximum byte value: " + Byte.MAX_VALUE) ;
 
-        short numberShort = 10;
+        short numberShort = (short)32768;
         System.out.println("Number Short = " + numberShort);
         System.out.println("Max Number Short = " + Short.MAX_VALUE);
         System.out.println("Min Number Short = " + Short.MIN_VALUE);
+
+        int intNumber = (int)2147483648L;
+        System.out.println("Number Int = " + intNumber);
+        System.out.println("Max Number Int = " + Integer.MAX_VALUE);
+        System.out.println("Min Number Int = " + Integer.MIN_VALUE);
+
+        long longNumber = 9223372036854775807L;
+        System.out.println("Number Long = " + longNumber);
+        System.out.println("Max Number Int = " + Long.MAX_VALUE);
+        System.out.println("Min Number Int = " + Long.MIN_VALUE);
+
 
     }
 
