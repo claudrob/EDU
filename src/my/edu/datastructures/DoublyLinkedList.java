412bf3cc969316b8be6dd466dea46aab37aebd5c
claudrob <crobinson@tec.mx>
2022-10-24 17:25:05 -0500

    Double LinkedList

--- src/my/edu/datastructures/DoublyLinkedList.java
+++ src/my/edu/datastructures/DoublyLinkedList.java
@@ -12,6 +12,82 @@ public class DoublyLinkedList {
         length = 1;
     }
 
+    public Node get(int index){
+        if(index < 0 || index >= length) return null;
+        Node temp = head;
+        if(index < (length / 2)) {
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+        }else {
+            temp = tail;
+            for(int i = length - 1; i > index; i--){
+                temp = temp.prev;
+            }
+        }
+        return temp;
+    }
+
+
+    public void append(int value){
+        Node newNode = new Node(value);
+
+        if(length == 0){
+            head = newNode;
+            tail = newNode;
+        }else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst(){
+        if(length == 0) return null;
+        Node temp = head;
+        if(length == 1){
+            head = null;
+            tail = null;
+        }else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
+
+    public void prepend(int value){
+        Node newNode = new Node(value);
+        if(length == 0){
+            head = newNode;
+            tail = newNode;
+        }else{
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node removeLast(){
+        if(length == 0) return null;
+        Node temp = tail;
+        if(length == 1){
+            head = null;
+            tail = null;
+        }
+        else {
+            tail = temp.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+        length--;
+        return temp;
+    }
+
+
     public void printList(){
         Node temp = head;
         while(temp != null){
--- src/my/edu/test/datastructures/TestDoublyLinkedList.java
+++ src/my/edu/test/datastructures/TestDoublyLinkedList.java
@@ -1,15 +1,107 @@
 package my.edu.test.datastructures;
 
 import my.edu.datastructures.DoublyLinkedList;
+import my.edu.datastructures.Node;
 
 public class TestDoublyLinkedList {
 
-    public static void main(String[] args) {
-        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+    private static DoublyLinkedList myDLL;
 
+    public static void main(String[] args) {
+
+
+        createNewDLL(1);
+        testAppend();
+        testRemoveLast();
+        testPrepend();
+        testRemoveFirst();
+        testGet();
+
+    }
+
+    public static void testRemoveLast(){
+        System.out.println("Testing: removedLast()");
+        Node removed = myDLL.removeLast();
+        System.out.println("Removed = " + removed);
+        printOutDLL();
+        removed = myDLL.removeLast();
+        System.out.println("Removed = " + removed);
+        printOutDLL();
+        removed = myDLL.removeLast();
+        System.out.println("Removed = " + removed);
+        printOutDLL();
+        removed = myDLL.removeLast();
+        System.out.println("Removed = " + removed);
+        printOutDLL();
+
+        removed = myDLL.removeLast();
+        System.out.println("Removed = " + removed);
+        printOutDLL();
+
+    }
+
+    private static void testPrepend(){
+        System.out.println("Testing: PREPend()");
+        myDLL.prepend(5);
+        myDLL.prepend(6);
+        myDLL.prepend(7);
+        myDLL.prepend(8);
+        printOutDLL();
+    }
+
+    private static void testRemoveFirst(){
+        System.out.println("Test: Remove First! --> DLL Before");
+        printOutDLL();
+        System.out.println("Removed: " + myDLL.removeFirst());
+//        System.out.println("Removed: " + myDLL.removeFirst());
+//        System.out.println("Removed: " + myDLL.removeFirst());
+//        System.out.println("Removed: " + myDLL.removeFirst());
+//        System.out.println("Removed: " + myDLL.removeFirst());
+        printOutDLL();
+    }
+
+
+    private static void testGet(){
+        System.out.println("Test: GET");
+
+        myDLL.prepend(8);
+        myDLL.prepend(9);
+        myDLL.prepend(10);
+        myDLL.append(4);
+        myDLL.append(3);
+        myDLL.append(2);
+        myDLL.append(1);
+        myDLL.append(0);
+
+        printOutDLL();
+        for(int i = 0; i < 10; i++){
+            System.out.println("i  = " + i  + ": " +  myDLL.get(i));
+        }
+
+    }
+
+    private static void testAppend(){
+        System.out.println("Testing: append()");
+        myDLL.append(2);
+        myDLL.append(3);
+        myDLL.append(4);
+        printOutDLL();
+
+
+    }
+
+    private static void createNewDLL(int value) {
+        myDLL = new DoublyLinkedList(value);
+
+        printOutDLL();
+    }
+
+    private static void printOutDLL() {
         myDLL.getHead();
         myDLL.getTail();
         myDLL.getLength();
         myDLL.printList();
     }
+
+
 }
