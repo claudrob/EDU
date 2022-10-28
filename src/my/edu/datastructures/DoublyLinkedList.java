1966fd653eefc25d6a0d8dc873b54627c7072561
claudrob <crobinson@tec.mx>
2022-10-28 09:12:16 -0500

    DoubleLinkedList practice

--- src/my/edu/datastructures/DoublyLinkedList.java
+++ src/my/edu/datastructures/DoublyLinkedList.java
@@ -37,6 +37,29 @@ public class DoublyLinkedList {
         return temp;
     }
 
+    public boolean insert(int index, int value){
+        if(index < 0 || index > length) return false;
+        if(index == 0){
+            prepend(value);
+            return true;
+        }
+        if(index == length){
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node before = get(index - 1);
+        Node after = before.next;
+
+        newNode.prev = before;
+        newNode.next = after;
+        before.next = newNode;
+        after.prev = newNode;
+
+        length++;
+        return true;
+    }
+
 
     public void append(int value){
         Node newNode = new Node(value);
@@ -50,6 +73,31 @@ public class DoublyLinkedList {
             tail = newNode;
         }
         length++;
+    }
+
+    public Node remove(int index){
+        if(index < 0 || index >= length) return null;
+        if(index == 0) return removeFirst();
+        if(index == (length - 1)) return removeLast();
+
+//        Node before = get(index - 1);
+//        Node temp = before.next;
+//        Node after = temp.next;
+
+//        before.next = after;
+//        after.prev = before;
+
+        Node temp = get(index);
+        temp.prev.next = temp.next;
+        temp.next.prev = temp.prev;
+
+        temp.prev = null;
+        temp.next = null;
+
+        length--;
+
+        return temp;
+
     }
 
     public Node removeFirst(){
