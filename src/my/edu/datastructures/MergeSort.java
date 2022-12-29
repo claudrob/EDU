42b9fdb6933765a8e55ceb1baf483a2b3ce4fda9
claudrob <crobinson@tec.mx>
2022-12-29 17:16:05 -0600

    Merge Sort.

--- src/my/edu/datastructures/MergeSort.java
+++ src/my/edu/datastructures/MergeSort.java
@@ -4,11 +4,41 @@ import java.util.Arrays;
 
 public class MergeSort {
 
+    //Break arrays in half
+    // Base case: when array.length is 1
+    // uses Merge to put arrays together
+
     public static void main(String [] args){
         int[] array1 = {1,3,7,8};
         int[] array2 = {2,4,5,6};
+
+
         System.out.print(Arrays.toString( merge(array1, array2) ));
+
+        int[] array = {3,1,4,2};
+        int [] sortedArray = mergeSort(array);
+
+        System.out.println("Merge Sort");
+        System.out.println("\nOriginal Array: " + Arrays.toString(array));
+        System.out.println("\nOriginal Array: " + Arrays.toString(sortedArray));
+
     }
+
+    public static int [] mergeSort(int [] array){
+//        int [] sorted = new int[array.length];
+
+        if(array.length == 1) return array;
+
+        int midIndex = array.length / 2;
+        int [] left = mergeSort(Arrays.copyOfRange(array, 0, midIndex));
+        int [] right = mergeSort(Arrays.copyOfRange(array, midIndex, array.length));
+
+        return merge(left, right);
+    }
+
+
+
+
 
 
     public static int[] merge(int [] array1, int [] array2){
