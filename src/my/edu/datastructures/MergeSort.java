10524e949e927012811800ac4e8906ca3f70a19f
Claud Robinson <crobinson4@west.com>
2022-12-29 16:13:46 -0600

    Merge sort.

---
+++ src/my/edu/datastructures/MergeSort.java
@@ -0,0 +1,46 @@
+package my.edu.datastructures;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void main(String [] args){
+        int[] array1 = {1,3,7,8};
+        int[] array2 = {2,4,5,6};
+        System.out.print(Arrays.toString( merge(array1, array2) ));
+    }
+
+
+    public static int[] merge(int [] array1, int [] array2){
+        int[] combined = new int[array1.length + array2.length];
+        int index = 0;
+        int i = 0;
+        int j = 0;
+
+        while(i < array1.length && j < array2.length){
+            if (array1[i] < array2[j]){
+                combined[index] = array1[i];
+                index++;
+                i++;
+            }else {
+                combined[index] = array2[j];
+                index++;
+                j++;
+            }
+        }
+        while(i < array1.length){
+            combined[index] = array1[i];
+            index++;
+            i++;
+        }
+        while(j < array2.length){
+            combined[index] = array2[j];
+            index++;
+            j++;
+        }
+
+        return combined;
+    }
+
+
+}
